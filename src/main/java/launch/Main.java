bfe6c5575e18f3c73933fa8dfa5d613d7128ed71
Ben Walch <b.walch@student.uibk.ac.at>
2019-07-15 20:19:37 +0200

    java backend, serving a web frontend

---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,63 @@
+package launch;
+
+import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+// import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceSet;
+import org.apache.catalina.webresources.EmptyResourceSet;
+
+// import java.io.FileReader;
+
+// import javax.script.ScriptEngine;
+// import javax.script.ScriptEngineManager;
+// import javax.script.ScriptException;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        // Define a web application context.
+        Context context = tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClassesFolder = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(context);
+
+        WebResourceSet resourceSet;
+        if (additionWebInfClassesFolder.exists()) {
+            resourceSet = new DirResourceSet(resources, "/WEB-INF/classes",
+                    additionWebInfClassesFolder.getAbsolutePath(), "/");
+            System.out.println(
+                    "loading WEB-INF resources from as '" + additionWebInfClassesFolder.getAbsolutePath() + "'");
+        } else {
+            resourceSet = new EmptyResourceSet(resources);
+        }
+
+        resources.addPreResources(resourceSet);
+        context.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+  <title>${title}</title>
+  <meta name="description" content="${description}">
+</head>
+
+<body>
+  <h1>It works!</h1>
+</body>
+
+</html>
