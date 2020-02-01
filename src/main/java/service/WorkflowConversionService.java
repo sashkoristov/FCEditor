6286b66dd244795aa88de452993f1ea3f318ee23
Ben Walch <ben@webstobe.ch>
2020-02-01 23:11:26 +0100

    generalized and improved workflow conversion service with the power of java reflection

--- src/main/java/service/WorkflowConversionService.java
+++ src/main/java/service/WorkflowConversionService.java
@@ -1,6 +1,9 @@
 package service;
 
 import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -9,10 +12,7 @@ import javax.xml.parsers.*;
 import javax.xml.xpath.*;
 
 import afcl.functions.*;
-import afcl.functions.objects.DataIns;
-import afcl.functions.objects.DataOuts;
-import afcl.functions.objects.DataOutsAtomic;
-import afcl.functions.objects.Section;
+import afcl.functions.objects.*;
 import org.xml.sax.InputSource;
 import org.w3c.dom.*;
 
@@ -40,7 +40,7 @@ public class WorkflowConversionService {
     }
 
     public static String toXml(Workflow w) {
-        return "";
+        return "Not implemented.";
     }
 
     protected static List<Function> generateFunctions(Node n, Document doc, List<Function> functionsList) throws Exception {
@@ -126,71 +126,175 @@ public class WorkflowConversionService {
         return functionsList;
     }
 
-    protected static AtomicFunction generateAtomicFunction(Element fnEl) {
-        AtomicFunction f = new AtomicFunction();
-        f.setName(fnEl.getAttribute("name"));
-        f.setType(fnEl.getAttribute("type"));
-        for (Node propNode = fnEl.getFirstChild(); propNode != null; propNode = propNode.getNextSibling()) {
-            if (propNode.getNodeName().equals("Array") && propNode instanceof Element) {
-                Element propEl = (Element) propNode;
-                switch (propEl.getAttribute("as")) {
-                    case "dataIns":
-                        List<DataIns> dataInsList = new ArrayList<>();
-                        for (Node dataInsNode = propNode.getFirstChild(); dataInsNode != null; dataInsNode = dataInsNode.getNextSibling()) {
-                            if (dataInsNode.getNodeName().equals("DataIns") && dataInsNode instanceof Element) {
-                                dataInsList.add(generateDataIns((Element)dataInsNode));
-                            }
-                        }
-                        f.setDataIns(dataInsList);
-                        break;
-                    case "dataOuts":
-                        List<DataOutsAtomic> dataOutsList = new ArrayList<>();
-                        for (Node dataOutsNode = propNode.getFirstChild(); dataOutsNode != null; dataOutsNode = dataOutsNode.getNextSibling()) {
-                            if (dataOutsNode.getNodeName().equals("DataOuts") && dataOutsNode instanceof Element) {
-                                dataOutsList.add(generateDataOutsAtomic((Element)dataOutsNode));
-                            }
-                        }
-                        f.setDataOuts(dataOutsList);
-                        break;
+    protected static void setPrimitiveFields(Element fnEl, Object afclObj) {
+        List<Field> primitiveFields = getAllDeclaredFields(new ArrayList<Field>(), afclObj.getClass());
+        for (Field primitiveField : primitiveFields) {
+            if (
+                    primitiveField.getType().equals(String.class) ||
+                            primitiveField.getType().equals(Integer.class) ||
+                            primitiveField.getType().equals(Boolean.class)
+            ) {
+                String value = fnEl.getAttribute(primitiveField.getName());
+                if (!value.isEmpty()) {
+                    try {
+                        primitiveField.setAccessible(true);
+                        primitiveField.set(afclObj, transformValue(value, primitiveField.getType()));
+                    } catch (IllegalAccessException iae) {
+                        System.out.println("Could not set primitive field " + primitiveField.getName() + " on " + afclObj.getClass().getName());
+                    }
                 }
             }
         }
+    }
+
+    protected static Object transformValue(String s, Class<?> type) {
+        if (type.equals(Boolean.class))
+            return s.equals("1") || s.toLowerCase().equals("true");
+        if (type.equals(Integer.class))
+            return Integer.parseInt(s);
+        return s;
+    }
+
+    protected static void setCommonProperties(Element fnEl, Object afclObj) {
+        for (Node propNode = fnEl.getFirstChild(); propNode != null; propNode = propNode.getNextSibling()) {
+            if (propNode instanceof Element) {
+                Element propEl = (Element) propNode;
+                Method setterMethod = null;
+                Object setterArg = null;
+                try {
+                    switch (propEl.getAttribute("as")) {
+                        case "constraints":
+                        case "properties":
+                            List<PropertyConstraint> pList = new ArrayList<>();
+                            for (Node pNode = propNode.getFirstChild(); pNode != null; pNode = pNode.getNextSibling()) {
+                                if (pNode.getNodeName().equals("Object") && pNode instanceof Element) {
+                                    PropertyConstraint pc = new PropertyConstraint();
+                                    setPrimitiveFields((Element)pNode, pc);
+                                    pList.add(pc);
+                                }
+                            }
+                            if (!pList.isEmpty()) {
+                                setterMethod = afclObj.getClass().getMethod("set" + propEl.getAttribute("as").substring(0, 1).toUpperCase() + propEl.getAttribute("as").substring(1), List.class);
+                                setterArg = pList;
+                            }
+                            break;
+                        case "dataIns":
+                            List<DataIns> dataInsList = new ArrayList<>();
+                            for (Node dataInsNode = propNode.getFirstChild(); dataInsNode != null; dataInsNode = dataInsNode.getNextSibling()) {
+                                if (dataInsNode.getNodeName().equals("DataIns") && dataInsNode instanceof Element) {
+                                    dataInsList.add(generateDataIns((Element)dataInsNode));
+                                }
+                            }
+                            if (!dataInsList.isEmpty()) {
+                                setterMethod = afclObj.getClass().getMethod("setDataIns", List.class);
+                                setterArg = dataInsList;
+                            }
+                            break;
+                        case "dataOuts":
+                            List<DataOuts> dataOutsList = new ArrayList<>();
+                            for (Node dataOutsNode = propNode.getFirstChild(); dataOutsNode != null; dataOutsNode = dataOutsNode.getNextSibling()) {
+                                if (dataOutsNode.getNodeName().equals("DataOuts") && dataOutsNode instanceof Element) {
+                                    dataOutsList.add(generateDataOuts((Element)dataOutsNode));
+                                }
+                            }
+                            if (!dataOutsList.isEmpty()) {
+                                setterMethod = afclObj.getClass().getMethod("setDataOuts", List.class);
+                                setterArg = dataOutsList;
+                            }
+                            break;
+                    }
+                    if (setterMethod != null) {
+                        setterMethod.invoke(afclObj, setterArg);
+                    }
+                }
+                catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ignored) {
+                    System.out.println("could not call method " + setterMethod.getName() + " on object ");
+                    System.out.println(afclObj);
+                }
+            }
+        }
+    }
+
+    protected static AtomicFunction generateAtomicFunction(Element fnEl) {
+        AtomicFunction f = new AtomicFunction();
+        setPrimitiveFields(fnEl, f);
+        setCommonProperties(fnEl, f);
         return f;
-    }
-
-    protected static DataIns generateDataIns(Element dataInsEl) {
-        DataIns dataIns = new DataIns();
-        dataIns.setName(dataInsEl.getAttribute("name"));
-        dataIns.setType(dataInsEl.getAttribute("type"));
-        dataIns.setSource(dataInsEl.getAttribute("source"));
-        return dataIns;
-    }
-
-    protected static DataOutsAtomic generateDataOutsAtomic(Element dataOutsEl) {
-        DataOutsAtomic dataOuts = new DataOutsAtomic();
-        dataOuts.setName(dataOutsEl.getAttribute("name"));
-        dataOuts.setType(dataOutsEl.getAttribute("type"));
-        return dataOuts;
     }
 
     protected static IfThenElse generateIfThenElse(Element iteEl) {
         IfThenElse ite = new IfThenElse();
+        Element compCondEl = getDirectChild(iteEl, "CompositeCondition");
+        if (compCondEl != null) {
+            Condition compCond = new Condition();
+            List<ACondition> conditionsList = new ArrayList<>();
+            Node condNodesArray = getDirectChild(compCondEl, "Array");
+            if (condNodesArray != null) {
+                NodeList condNodes = condNodesArray.getChildNodes();
+                for (int i = 0; i < condNodes.getLength(); i++) {
+                    Node condNode = condNodes.item(i);
+                    if (condNode instanceof Element) {
+                        ACondition cond = new ACondition();
+                        setPrimitiveFields((Element)condNode, cond);
+                        conditionsList.add(cond);
+                    }
+                }
+            }
+            if (!conditionsList.isEmpty())
+                compCond.setConditions(conditionsList);
+            setPrimitiveFields(compCondEl, compCond);
+            ite.setCondition(compCond);
+        }
+        setPrimitiveFields(iteEl, ite);
+        setCommonProperties(iteEl, ite);
         return ite;
     }
 
     protected static Switch generateSwitch(Element switchEl) {
         Switch sw = new Switch();
+        Element dataEvalEl = getDirectChild(switchEl, "DataEval");
+        if (dataEvalEl != null) {
+            DataEval dataEval = new DataEval();
+            setPrimitiveFields(dataEvalEl, dataEval);
+            sw.setDataEval(dataEval);
+        }
+        setPrimitiveFields(switchEl, sw);
+        setCommonProperties(switchEl, sw);
         return sw;
     }
 
     protected static Parallel generateParallel(Element parEl) {
         Parallel par = new Parallel();
+        setPrimitiveFields(parEl, par);
+        setCommonProperties(parEl, par);
         return par;
     }
 
     protected static ParallelFor generateParallelFor(Element parForEl) {
         ParallelFor parFor = new ParallelFor();
+        Element loopCounterEl = getDirectChild(parForEl, "LoopCounter");
+        if (loopCounterEl != null) {
+            LoopCounter loopCounter = new LoopCounter();
+            setPrimitiveFields(loopCounterEl, loopCounter);
+            parFor.setLoopCounter(loopCounter);
+        }
+        setPrimitiveFields(parForEl, parFor);
+        setCommonProperties(parForEl, parFor);
         return parFor;
+    }
+
+    protected static DataIns generateDataIns(Element dataInsEl) {
+        DataIns dataIns = new DataIns();
+        setPrimitiveFields(dataInsEl, dataIns);
+        setCommonProperties(dataInsEl, dataIns);
+        return dataIns;
+    }
+
+    protected static DataOuts generateDataOuts(Element dataOutsEl) {
+        DataOuts dataOuts = new DataOuts();
+        setPrimitiveFields(dataOutsEl, dataOuts);
+        setCommonProperties(dataOutsEl, dataOuts);
+        return dataOuts;
     }
 
     protected static Element getDirectChild(Element parent, String tagName)
@@ -223,6 +327,16 @@ public class WorkflowConversionService {
         DocumentBuilder builder = factory.newDocumentBuilder();
 
         return builder.parse(new InputSource(new StringReader(xml)));
+    }
+
+    public static List<Field> getAllDeclaredFields(List<Field> fields, Class<?> type) {
+        fields.addAll(Arrays.asList(type.getDeclaredFields()));
+
+        if (type.getSuperclass() != null) {
+            getAllDeclaredFields(fields, type.getSuperclass());
+        }
+
+        return fields;
     }
 
 }
